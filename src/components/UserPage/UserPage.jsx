3617e6ace995f15fb25a0b29b030dde5f8daa38a
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-03-14 14:55:56 -0500

    Began Task Reducer Building

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -1,16 +1,22 @@
-import React from 'react';
+import React, { useEffect, useState } from 'react';
 import LogOutButton from '../LogOutButton/LogOutButton';
-import {useSelector} from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
-  const user = useSelector((store) => store.user);
+  // const user = useSelector((store) => store.user);
+  const dispatch = useDispatch();
+    const tasks = useSelector(store => store.tasks);
+  useEffect(() => {
+    dispatch({ type: 'FETCH_TASKS' });
+  }, []);
   return (
     <div className="container">
-      <h2>Welcome, {user.username}!</h2>
-      <p>Your ID is: {user.id}</p>
+      {/* <h2>Welcome, {user.username}!</h2> */}
+      {/* <p>Your ID is: {user.id}</p> */}
       <LogOutButton className="btn" />
     </div>
+    
   );
 }
 
