023c17734fba432b005076f541f19e429fa768c7
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-03-15 13:20:10 -0500

    Delete ID logging

--- src/components/TaskItem/TaskItem.jsx
+++ src/components/TaskItem/TaskItem.jsx
@@ -4,11 +4,21 @@ import { useDispatch, useSelector } from 'react-redux';
 
 function TaskItem({task}){
 
+    function handleDelete(){
+        console.log(task.id);
+    }
+
     return (
         <>
         <tr>
         <td>{task.name}</td>
         <td>{task.description}</td>
+        <td>
+            <button>Start</button>
+            <button onClick={handleDelete}>Delete</button>
+            <button>Edit</button>
+            
+        </td>
         </tr>
         
         
