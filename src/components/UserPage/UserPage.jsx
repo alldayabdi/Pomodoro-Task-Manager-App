627273be188c9f6383d22c42d6d843c003939be1
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-03-15 09:56:36 -0500

    Form inputs built

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -6,11 +6,26 @@ import './UserPage.css'
 function UserPage() {
   // this component doesn't do much to start, just renders some user reducer info to the DOM
   // const user = useSelector((store) => store.user);
+  const [description, setDescription] = useState('');
+  const [taskName, setTaskName] = useState('');
+
   const dispatch = useDispatch();
-    const tasks = useSelector(store => store.taskReducer);
+
+  const handleSubmit = () => {
+    // let bundledObject = {
+    //   description: description,
+    //   taskName: taskName,
+    //   user_id: userId
+    // }
+    console.log('Clicked!!');
+  }
+
+  const tasks = useSelector(store => store.taskReducer);
+
   useEffect(() => {
     dispatch({ type: 'FETCH_TASKS' });
   }, []);
+
   return (
     <div className="app-container">
       {/* <h2>Welcome, {user.username}!</h2> */}
@@ -20,26 +35,45 @@ function UserPage() {
         <thead>
           <tr>
             <th>Task Name</th>
-            <th>Task Notes</th>
-            </tr>
-            <tbody></tbody>
-            
-                {tasks.map((task,i) => {
-                    return (
-                        <tr key ={i} >
-                          
-                          <td>{task.name}</td>
-                          <td>{task.description}</td>
-                          
-                        </tr>
-                    )
-                })}
-                
-                </thead>
-            </table>
+            <th>Task Description/Notes</th>
+            <th>Status</th>
+          </tr>
+        </thead>
+        <tbody>
+
+          {tasks.map((task, i) => {
+            return (
+              <tr key={i} >
+
+                <td>{task.name}</td>
+                <td>{task.description}</td>
+                <td>{task.status}</td>
+
+              </tr>
+            )
+          })}
+        </tbody>
+
+
+      </table>
+      <form onSubmit={handleSubmit}>
+      <input
+          type="text"
+          placeholder="Enter Task Name"
+          onChange={(event) => setTaskName(event.target.value)}
+        />
+
+        <input
+          type="text"
+          placeholder="Enter Task Description"
+          onChange={(event) => setDescription(event.target.value)}
+        />
+    
+        <button type="submit">Add to Task List!</button>
+      </form>
       {/* <LogOutButton className="btn" /> */}
     </div>
-    
+
   );
 }
 
