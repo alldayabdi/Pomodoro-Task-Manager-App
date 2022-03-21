313cdf82a4996b637b44c9e87e40f3db4bba9daf
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-03-21 13:35:25 -0500

    Started building out work, short break, long break buttons and functions using Pomodoro Clock Tutorial

--- src/StartTask/StartTask.jsx
+++ src/StartTask/StartTask.jsx
@@ -1,20 +1,54 @@
-import React from 'react'
+import React, { useContext } from 'react'
 import { useDispatch, useSelector } from 'react-redux';
 import holdStartDetails from '../redux/reducers/startTaskReducer';
 import './StartTask.css'
+import { SettingContext } from '../components/Pomodoro Clock/ContextSettings';
 import Settings from '../components/Pomodoro Clock/Settings';
 import CountDownCircle from '../components/Pomodoro Clock/Countdown'
+import Button from '../components/Pomodoro Clock/Button';
 const StartTask = () => {
+  const {pomodoro, executing, setCurrentTimer} = useContext(SettingContext)
     
     const startTask = useSelector(store => store.holdStartDetails);
     console.log(holdStartDetails.id);
   return (
       <>
       <div className='container'>
-    <Settings/>
+   
     {/* <CountDownCircle /> */}
-    <p><strong>{startTask.name}</strong></p>
-    <p>{startTask.description}</p>
+    {/* <p><strong>{startTask.name}</strong></p>
+    <p>{startTask.description}</p> */}
+   {pomodoro !== 0 ? 
+   <Settings/>
+   :
+    <>
+   <ul className='labels'>
+    <li>
+    <Button 
+              title="Work" 
+              activeClass={executing.active === 'work' ? 'active-label' : undefined} 
+              calledFunction={() => setCurrentTimer('work')} 
+            />
+    </li>
+
+    <li>
+            <Button 
+              title="Short Break" 
+              activeClass={executing.active === 'short' ? 'active-label' : undefined} 
+              calledFunction={() => setCurrentTimer('short')} 
+            />
+          </li>
+          <li>
+            <Button 
+              title="Long Break" 
+              activeClass={executing.active === 'long' ? 'active-label' : undefined} 
+              calledFunction={() => setCurrentTimer('long')} 
+            />
+          </li>
+
+   </ul>
+    </>
+  }
     </div>
     </>
   )
--- src/components/Pomodoro Clock/ContextSettings.jsx
+++ src/components/Pomodoro Clock/ContextSettings.jsx
@@ -5,6 +5,14 @@ const SettingsContextProvider = (props) => {
     const [pomodoro, setPomodoro] = useState(0)
     const [executing, setExecuting] = useState({})
     const [startAnimate, setStartAnimate] = useState(false)
+
+    function setCurrentTimer(active_state){
+        updateExecute({
+            ...executing,
+            active: active_state
+        })
+        setTimerTime(executing)
+    }
     function startTimer() {
         setStartAnimate(true)
     }
@@ -16,15 +24,56 @@ const SettingsContextProvider = (props) => {
             setStartAnimate(false)
             }
 
+            const SettingsBtn = () =>{
+                setExecuting({})
+                setPomodoro(0)
+            }
+
             const updateExecute = updatedSettings => {
                 setExecuting(updatedSettings)
+                setTimerTime(updatedSettings)
                 
             }
-        
 
+            const setTimerTime = evaluate =>{
+                switch (evaluate.active) {
+                    case' work':
+                        setPomodoro(evaluate.work)
+                        break;
+                    
+                    case 'short':
+                        setPomodoro(evaluate.short)
+                    break;
+
+                    case 'long':
+                    setPomodoro(evaluate.long)
+
+                    default:
+                        setPomodoro(0)
+                        break;
+                }
+            }
+        
+const children =({remainingTimer}) =>{
+    const minutes = Math.floor(remainingTimer/60)
+    const seconds = remainingTimer % 60
+
+    return `${minutes}m ${seconds}`
+}
 
   return (
-    <SettingContext.Provider value={{stopTimer, updateExecute}} >
+    <SettingContext.Provider
+     value={{
+         children,
+         executing,
+         updateExecute,
+         setCurrentTimer,
+         SettingsBtn,
+         pauseTimer,
+         startTimer,
+         startAnimate,
+         pomodoro,
+         stopTimer, updateExecute}} >
         {props.children}
     </SettingContext.Provider >
   )
