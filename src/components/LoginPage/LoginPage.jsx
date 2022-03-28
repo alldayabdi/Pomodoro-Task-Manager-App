d15a3c125a62f8f4d89aa5a74751a5037c685903
Abdikarim Ibrahim <abdikarimibrahim@hotmail.com>
2022-03-28 14:32:41 -0500

    Added styling

--- src/components/LoginPage/LoginPage.jsx
+++ src/components/LoginPage/LoginPage.jsx
@@ -12,7 +12,7 @@ function LoginPage() {
       <LoginForm />
 
       <center>
-        <button
+        <button id='register'
           type="button"
           className="btn btn_asLink"
           onClick={() => {
